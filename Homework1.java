7e03d2b94fa8f49f018a2942f2a44cb7597cce87
Daniel Feliciano <felicdan@berkeleyprep.org>
2018-09-06 15:25:23 -0400

    It is done.

--- Homework1.java
+++ Homework1.java
@@ -1,17 +1,17 @@
 /*
- * First and Last Names
+ * Daniel Feliciano
  */
 
 import java.awt.Rectangle;
 
-public class Homework1 {
+public class Homework1{
 
 	/* Fix the error(s) in this method so that it runs properly
 	 */
 	public static int problem1() {
 		int mystery = 1;
 		mystery = mystery + 1;
-		int mystery = 1 - 2 * mystery;
+		mystery = 1-2 * mystery;
 		return mystery;
 	}
 
@@ -19,25 +19,30 @@ public class Homework1 {
 	 * height of h
 	 */
 	public static Rectangle problem2(int w, int h) {
+Rectangle rect= new Rectangle(50, 50, w, h);
+	return rect;
 
 	}
 
 	/* Declare an integer, assign it a value of 5, then return it
 	 */
 	public static int problem3() {
-
+		int x = 5;
+		return x;
 	}
 
 	/* Declare and return a String containing the phrase "Computer Science"
 	 */
 	public static String problem4() {
-
-	}
+			String x = "Computer Science";
+			return x;
+		}
 
 	/* Add 5 to num and return the new value
 	 */
 	public static int problem5(int num) {
-
+		num = num + 5;
+		return num;
 	}
 
 }
